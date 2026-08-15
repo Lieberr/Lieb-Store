dec4d31321c95be33c3ed86481861154a9893397
Lieberr <gustavoliebfigueira@gmail.com>
2026-08-15 14:52:07 -0300

    NavBar atualizada

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -7,25 +7,66 @@ import Search from './search';
 
 const Header = () => {
     return (
-        <header className='w-full border-b'>
-            <div className='wrapper flex-between'>
-                <div className='flex-start'>
-                    <CategoryDropDrawer />
-                    <Link href='/' className='flex-start ml-4'>
-                        <Image src='/images/logo.svg' alt={`${APP_NAME} logo`}
-                        height={48} width={48} priority={true} />
-                        <span className='hidden lg:block font-bold text-2xl ml-3'>
-                            {APP_NAME}
-                        </span>
-                    </Link>
+        <>
+            <header
+                className="
+                    fixed
+                    inset-x-0
+                    top-0
+                    z-50
+                    h-16
+                    w-full
+                    border-b
+                    border-border/50
+                    bg-background/95
+                    backdrop-blur
+                    supports-[backdrop-filter]:bg-background/80
+                "
+            >
+                <div className="wrapper h-full">
+                    <div className="flex h-full items-center justify-between gap-4">
+                        <div className="flex shrink-0 items-center gap-3">
+                            <CategoryDropDrawer />
+                            <Link
+                                href="/"
+                                className="group flex items-center gap-2"
+                            >
+                                <Image
+                                    src="/images/logo.svg"
+                                    alt={`${APP_NAME} logo`}
+                                    height={38}
+                                    width={38}
+                                    priority
+                                    className="
+                                        transition-transform
+                                        duration-200
+                                        group-hover:scale-105
+                                    "
+                                />
+                                <span
+                                    className="
+                                        hidden
+                                        sm:block
+                                        text-xl
+                                        lg:text-2xl
+                                        font-bold
+                                        tracking-tight
+                                    "
+                                >
+                                    {APP_NAME}
+                                </span>
+                            </Link>
+                        </div>
+                        <div className="hidden md:flex flex-1 justify-center px-6">
+                            <Search />
+                        </div>
+                        <Menu />
+                    </div>
                 </div>
-                <div className='hidden md:block'>
-                    <Search />
-                </div>
-                <Menu />
-            </div>
-        </header>
-    )
-}
- 
+            </header>
+            <div className="h-16 mb-8"  />
+        </>
+    );
+};
+
 export default Header;
