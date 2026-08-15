5541d9e1359a935339239d59db4d0b6d9884e842
Lieberr <gustavoliebfigueira@gmail.com>
2026-08-15 15:23:09 -0300

    NavBar fixed

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -7,65 +7,67 @@ import Search from './search';
 
 const Header = () => {
     return (
-        <>
-            <header
-                className="
-                    fixed
-                    inset-x-0
-                    top-0
-                    z-50
-                    h-16
-                    w-full
-                    border-b
-                    border-border/50
-                    bg-background/95
-                    backdrop-blur
-                    supports-[backdrop-filter]:bg-background/80
-                "
-            >
-                <div className="wrapper h-full">
-                    <div className="flex h-full items-center justify-between gap-4">
-                        <div className="flex shrink-0 items-center gap-3">
-                            <CategoryDropDrawer />
-                            <Link
-                                href="/"
-                                className="group flex items-center gap-2"
+        <header
+            className="
+                w-full
+                h-16
+                border-b
+                border-border/50
+                bg-background
+            "
+        >
+            <div className="wrapper h-full">
+                <div className="flex h-full items-center justify-between gap-4">
+
+                    {/* Logo + Categories */}
+                    <div className="flex shrink-0 items-center gap-3">
+
+                        <CategoryDropDrawer />
+
+                        <Link
+                            href="/"
+                            className="group flex items-center gap-2"
+                        >
+                            <Image
+                                src="/images/logo.svg"
+                                alt={`${APP_NAME} logo`}
+                                height={38}
+                                width={38}
+                                priority
+                                className="
+                                    transition-transform
+                                    duration-200
+                                    group-hover:scale-105
+                                "
+                            />
+
+                            <span
+                                className="
+                                    hidden
+                                    sm:block
+                                    text-xl
+                                    lg:text-2xl
+                                    font-bold
+                                    tracking-tight
+                                "
                             >
-                                <Image
-                                    src="/images/logo.svg"
-                                    alt={`${APP_NAME} logo`}
-                                    height={38}
-                                    width={38}
-                                    priority
-                                    className="
-                                        transition-transform
-                                        duration-200
-                                        group-hover:scale-105
-                                    "
-                                />
-                                <span
-                                    className="
-                                        hidden
-                                        sm:block
-                                        text-xl
-                                        lg:text-2xl
-                                        font-bold
-                                        tracking-tight
-                                    "
-                                >
-                                    {APP_NAME}
-                                </span>
-                            </Link>
-                        </div>
-                        <div className="hidden md:flex flex-1 justify-center px-6">
-                            <Search />
-                        </div>
-                        <Menu />
+                                {APP_NAME}
+                            </span>
+                        </Link>
+
                     </div>
+
+                    {/* Search */}
+                    <div className="hidden md:flex flex-1 justify-center px-6">
+                        <Search />
+                    </div>
+
+                    {/* Menu */}
+                    <Menu />
+
                 </div>
-            </header>
-            <div className="h-16 mb-8"  />
-        </>
+            </div>
+        </header>
     );
 };
 
